d6e79b1b50f7c88e077c971e742d6e914391e868
M. Fareed Arif <fareed.arif@yahoo.com>
2020-08-07 18:14:49 -0500

    updated kind2 X Parser

--- KXParser.py
+++ KXParser.py
@@ -1,6 +1,10 @@
 '''
 Created on Nov 23, 2018
 
+Parse kind2 output in XML format. 
+
+#kind2 example.lus -xml egs/cex_output.xml
+
 @author: marif
 '''
 import sys
@@ -12,19 +16,14 @@ def parse_ct(example, ct):
            
     for node in example:
         if node.tag == 'Node':
-#            print(node.attrib['name'])
-#            ct.append([]) 
             ct.append(node.attrib['name'])
             parse_ct(node, ct)
         if node.tag == 'Stream':
-        #    if node.attrib['class'] == 'input' or node.attrib['class'] == 'output':
-#            print(node.attrib['name'],node.attrib['type'])
             ct.append(node.attrib['name'])
             ct.append(node.attrib['type'])
             if node.attrib['class'] == 'input' or node.attrib['class'] == 'output' :
                 parse_ct(node, ct)
         if node.tag == 'Value':
-#            print(node.attrib['instant'],node.text)
             ct.append(node.attrib['instant'])
             ct.append(node.text)
             parse_ct(node, ct)   
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     try:
         k_file = sys.argv[1]
     except IndexError:
-        k_file = 'egs/hawk-result-1.xml'        
+        k_file = 'egs/cex_output.xml'        
  
     tree = ET.ElementTree(file=k_file)
     root = tree.getroot()
@@ -49,28 +48,31 @@ if __name__ == '__main__':
     ct = []
     
     for element in tree.iter(tag='Property'):
-#        if element.tag == 'name':
+
         p_name = element.attrib['name']
         result.append([])
-#        print(element.attrib['name'])
+
         result[p_index].append(p_index+1)
         result[p_index].append(p_name)
         for subelem in element:
             if subelem.tag == 'Answer':
                 p_status = subelem.text
                 result[p_index].append(p_status)
-#                print(subelem.attrib['source'], )
+
                 if subelem.text == 'valid':
                     result[p_index].append(True)
                 else:
                     result[p_index].append(False)
-#                    c_example = subelem.getroot()
+
                     for celement in element:
                         if celement.tag == 'CounterExample':
                             result[p_index].append(parse_ct(celement,[]))
         
-        print(result[p_index])
-#        print(ct)
+        if(result[p_index][3]):
+            print("VALID:", result[p_index])
+        else:
+            print("CEX:", result[p_index])
+        
         p_index += 1
         
     pass
